c74eb052ed0a21fd49886294701762515abc6a5b
André Seixas <andrefil.seixas@gmail.com>
2021-09-03 16:23:24 +0100

    Arranjado código

--- public/javascript/modules.js
+++ public/javascript/modules.js
@@ -14,26 +14,30 @@ function btnCriar() {
     return confirm("Deseja criar este produto?");
 }
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+function openCriarAlimento() {
+    document.getElementById("criarAlimento").style.width = "100%";
 }
 
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+function openEditarAlimento() {
+    document.getElementById("editarAlimento").style.width = "100%";
 }
 
-function openNavInventario(id,nome,obs,estado) {
-    document.getElementById("id").value=id;
-    document.getElementById("nome").innerText=nome;
+function closeCriarAlimento() {
+    document.getElementById("criarAlimento").style.width = "0%";
+}
+
+function closeEditarAlimento() {
+    document.getElementById("editarAlimento").style.width = "0%";
+}
+
+function openNavInventario(id, nome, obs, estado) {
+    document.getElementById("id").value = id;
+    document.getElementById("nome").innerText = nome;
     document.getElementById("observacoes").value = obs;
-    if(estado==0){
-        document.getElementById("estado").checked =false;
+    if(estado == 0){
+        document.getElementById("estado").checked = false;
     }else{
-        document.getElementById("estado").checked =true;
+        document.getElementById("estado").checked = true;
     }
-    document.getElementById("myNav").style.width = "100%";
-}
-
-function btnGuardar() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("editarAlimento").style.width = "100%";
 }
