322cffdd6ca459a359d581ee1fe28816d1a66079
root <root@peopleforpeople-01.servidorescloud.pt>
2021-11-22 02:22:24 +0000

    Adição do pw da db

--- database.js
+++ database.js
@@ -3,6 +3,6 @@ const mysql = require('mysql2');
 module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Sobreda1135!',
     database: 'apoiofraterno',
 });
